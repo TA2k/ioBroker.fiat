aaa915d06b35ba40a9648a876e1f7afbffd4c2cf
TA2k <tombox2020@gmail.com>
2021-07-13 23:05:12 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,378 @@
+"use strict";
+
+/*
+ * Created with @iobroker/create-adapter v1.34.1
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require("@iobroker/adapter-core");
+const axios = require("axios");
+const axiosCookieJarSupport = require("axios-cookiejar-support").default;
+const tough = require("tough-cookie");
+const crypto = require("crypto");
+const aws4 = require("aws4");
+const { extractKeys } = require("./lib/extractKeys");
+// Load your modules here, e.g.:
+// const fs = require("fs");
+
+class Fiat extends utils.Adapter {
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    constructor(options) {
+        super({
+            ...options,
+            name: "fiat",
+        });
+        this.on("ready", this.onReady.bind(this));
+        // this.on("stateChange", this.onStateChange.bind(this));
+        this.on("unload", this.onUnload.bind(this));
+    }
+
+    /**
+     * Is called when databases are connected and adapter received configuration.
+     */
+    async onReady() {
+        axiosCookieJarSupport(axios);
+        this.cookieJar = new tough.CookieJar();
+        this.idArray = [];
+        this.refreshTokenInterval = null;
+        this.extractKeys = extractKeys;
+        this.setState("info.connection", false, true);
+        this.login()
+            .then(() => {
+                this.setState("info.connection", true, true);
+                this.log.info("Login successful");
+            })
+            .catch(() => {
+                this.log.error("Login failed");
+                this.setState("info.connection", false, true);
+            });
+    }
+
+    login() {
+        return new Promise((resolve, reject) => {
+            axios({
+                method: "post",
+                jar: this.cookieJar,
+                url: "https://loginmyuconnect.fiat.com/accounts.login",
+                headers: {
+                    accept: "*/*",
+                    "content-type": "application/x-www-form-urlencoded",
+                    origin: "https://myuconnect.fiat.com",
+                    "accept-language": "de-de",
+                    "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Mobile/15E148 Safari/604.1",
+                    referer: "https://myuconnect.fiat.com/de/de/login",
+                },
+                data: [
+                    "loginID=" + this.config.user,
+                    "password=" + this.config.password,
+                    "sessionExpiration=86400",
+                    "targetEnv=jssdk",
+                    "include=profile,data,emails,subscriptions,preferences,",
+                    "includeUserInfo=true",
+                    "loginMode=standard",
+                    "lang=de0de",
+                    'riskContext={"b0":52569,"b2":8,"b5":1}',
+                    "APIKey=3_mOx_J2dRgjXYCdyhchv3b5lhi54eBcdCTX4BI8MORqmZCoQWhA0mV2PTlptLGUQI",
+                    "source=showScreenSet",
+                    "sdk=js_latest",
+                    "authMode=cookie",
+                    "pageURL=https://myuconnect.fiat.com/de/de/login",
+                    "sdkBuild=12234",
+                    "format=json",
+                ].join("&"),
+            })
+                .then((response) => {
+                    if (!response.data) {
+                        this.log.error("Login failed maybe incorrect login information");
+                        reject();
+                        return;
+                    }
+                    this.log.debug(JSON.stringify(response.data));
+                    if (!response.data.sessionInfo) {
+                        this.log.error("sessionInfo missing");
+                        reject();
+                        return;
+                    }
+                    this.loginToken = response.data.sessionInfo.login_token;
+                    this.UID = response.data.userInfo.UID;
+                    this.extractKeys(this, "general", response.data);
+                    axios({
+                        method: "get",
+                        jar: this.cookieJar,
+                        url: [
+                            "https://loginmyuconnect.fiat.com/accounts.getJWT?fields=profile.firstName,profile.lastName,profile.email,country,locale,data.disclaimerCodeGSDP,data.GSDPisVerified",
+                            "APIKey=3_mOx_J2dRgjXYCdyhchv3b5lhi54eBcdCTX4BI8MORqmZCoQWhA0mV2PTlptLGUQI",
+                            "sdk=js_latest",
+                            "login_token=" + this.loginToken,
+                            "authMode=cookie",
+                            "pageURL=https://myuconnect.fiat.com/de/de/dashboard",
+                            "sdkBuild=12234",
+                            "format=json",
+                        ].join("&"),
+                        headers: {
+                            accept: "*/*",
+                            origin: "https://myuconnect.fiat.com",
+                            "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Mobile/15E148 Safari/604.1",
+                            "accept-language": "de-de",
+                            referer: "https://myuconnect.fiat.com/de/de/dashboard",
+                        },
+                    })
+                        .then((response) => {
+                            if (!response.data) {
+                                this.log.error("JWT failed maybe incorrect login information");
+                                reject();
+                                return;
+                            }
+                            this.log.debug(JSON.stringify(response.data));
+                            if (!response.data.id_token) {
+                                this.log.error("id_token missing");
+                                reject();
+                                return;
+                            }
+                            this.idToken = response.data.id_token;
+                            axios({
+                                method: "post",
+                                url: "https://authz.sdpr-01.fcagcv.com/v2/cognito/identity/token",
+                                headers: {
+                                    "content-type": "application/json",
+                                    "x-clientapp-version": "1.0",
+                                    clientrequestid: this.randomString(16),
+                                    accept: "*/*",
+                                    locale: "de_de",
+                                    "x-api-key": "qLYupk65UU1tw2Ih1cJhs4izijgRDbir2UFHA3Je",
+                                    "accept-language": "de-de",
+                                    origin: "https://myuconnect.fiat.com",
+                                    "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Mobile/15E148 Safari/604.1",
+                                    referer: "https://myuconnect.fiat.com/de/de/dashboard",
+                                    "x-originator-type": "web",
+                                },
+                                data: JSON.stringify({
+                                    gigya_token: this.idToken,
+                                }),
+                            })
+                                .then((response) => {
+                                    if (!response.data) {
+                                        this.log.error("fca failed maybe incorrect login information");
+                                        reject();
+                                        return;
+                                    }
+                                    this.log.debug(JSON.stringify(response.data));
+                                    if (!response.data.Token) {
+                                        this.log.error("Token missing");
+                                        reject();
+                                        return;
+                                    }
+                                    this.token = response.data.Token;
+                                    const data = JSON.stringify({
+                                        IdentityId: "eu-west-1:6f7882c6-4775-47b8-9eae-298ea0a088e6",
+                                        Logins: {
+                                            "cognito-identity.amazonaws.com": this.token,
+                                        },
+                                    });
+                                    axios({
+                                        method: "post",
+                                        url: "https://cognito-identity.eu-west-1.amazonaws.com/",
+                                        headers: {
+                                            "content-type": "application/x-amz-json-1.1",
+                                            accept: "*/*",
+                                            "x-amz-user-agent": "aws-sdk-js/2.283.1 callback",
+                                            "accept-language": "de-de",
+                                            origin: "https://myuconnect.fiat.com",
+                                            "x-amz-content-sha256": crypto.createHash("sha256").update(data).digest("hex"),
+                                            "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Mobile/15E148 Safari/604.1",
+                                            referer: "https://myuconnect.fiat.com/de/de/dashboard",
+                                            "x-amz-target": "AWSCognitoIdentityService.GetCredentialsForIdentity",
+                                        },
+                                        data: data,
+                                    })
+                                        .then((response) => {
+                                            if (!response.data) {
+                                                this.log.error("amz failed maybe incorrect login information");
+                                                reject();
+                                                return;
+                                            }
+                                            this.log.debug(JSON.stringify(response.data));
+                                            this.amz = JSON.parse(response.data);
+                                            if (!this.amz.Credentials) {
+                                                this.log.error("Credentials missing");
+                                                reject();
+                                                return;
+                                            }
+
+                                            this.refreshTokenInterval = setInterval(() => {
+                                                // this.refreshToken().catch((error) => {
+                                                //     this.log.error("Refresh token failed");
+                                                // });
+                                            }, 60 * 60 * 1000);
+                                            resolve();
+                                        })
+                                        .catch((error) => {
+                                            this.log.error(error);
+                                            this.log.error("amz Token failed");
+                                            error.response && this.log.error(JSON.stringify(error.response.data));
+                                            reject();
+                                        });
+                                })
+                                .catch((error) => {
+                                    this.log.error(error);
+                                    this.log.error("fca Token failed");
+                                    error.response && this.log.error(JSON.stringify(error.response.data));
+                                    reject();
+                                });
+                        })
+                        .catch((error) => {
+                            this.log.error(error);
+                            this.log.error("JWT Token failed");
+                            error.response && this.log.error(JSON.stringify(error.response.data));
+                            reject();
+                        });
+                })
+                .catch((error) => {
+                    this.log.error(error);
+                    this.log.error("Login failed");
+                    error.response && this.log.error(JSON.stringify(error.response.data));
+                    reject();
+                });
+        });
+    }
+    getVehicles() {
+        return new Promise((resolve, reject) => {
+            // aws4.sign() will sign and modify these options, ready to pass to http.request
+            const signed = aws4.sign(
+                {
+                    host: "channels.sdpr-01.fcagcv.com",
+                    path: "/v4/accounts/" + this.UID + "/vehicles?stage=ALL",
+                    service: "execute-api",
+                    method: "GET",
+                    region: "eu-west-1",
+                    headers: {
+                        Host: "channels.sdpr-01.fcagcv.com",
+                        "content-type": "application/json",
+                        "x-clientapp-version": "1.0",
+                        clientrequestid: this.randomString(16),
+                        accept: "application/json, text/plain, */*",
+                        "x-amz-date": this.amzDate(),
+                        "x-amz-security-token": this.amz.Credentials.SessionToken,
+                        locale: "de_de",
+                        "x-api-key": "qLYupk65UU1tw2Ih1cJhs4izijgRDbir2UFHA3Je",
+                        "accept-language": "de-de",
+                        "x-clientapp-name": "CWP",
+                        origin: "https://myuconnect.fiat.com",
+                        "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Mobile/15E148 Safari/604.1",
+                        referer: "https://myuconnect.fiat.com/de/de/dashboard",
+                        "x-originator-type": "web",
+                    },
+                },
+                { accessKeyId: this.amz.Credentials.AccessKeyId, secretAccessKey: this.amz.Credentials.SecretKey }
+            );
+            this.log.debug(signed);
+            axios({
+                method: "get",
+                host: "channels.sdpr-01.fcagcv.com",
+                path: "/v4/accounts/" + this.UID + "/vehicles?stage=ALL",
+                service: "execute-api",
+                jar: this.cookieJar,
+                url: "https://channels.sdpr-01.fcagcv.com/v4/accounts/" + this.UID + "/vehicles?stage=ALL",
+                headers: {
+                    Host: "channels.sdpr-01.fcagcv.com",
+                    "content-type": "application/json",
+                    "x-clientapp-version": "1.0",
+                    clientrequestid: this.randomString(16),
+                    accept: "application/json, text/plain, */*",
+
+                    "x-amz-date": this.amzDate(),
+                    "x-amz-security-token": this.amz.Credentials.SessionToken,
+                    locale: "de_de",
+                    "x-api-key": "qLYupk65UU1tw2Ih1cJhs4izijgRDbir2UFHA3Je",
+                    "accept-language": "de-de",
+                    "x-clientapp-name": "CWP",
+                    origin: "https://myuconnect.fiat.com",
+                    "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Mobile/15E148 Safari/604.1",
+                    referer: "https://myuconnect.fiat.com/de/de/dashboard",
+                    "x-originator-type": "web",
+                },
+            })
+                .then((response) => {
+                    if (!response.data) {
+                        this.log.error("Get vehicles failed");
+                        reject();
+                        return;
+                    }
+                    this.log.debug(JSON.stringify(response.data));
+                    response.data.vehicles.forEach(async (element) => {
+                        this.idArray.push(element.vin);
+                        await this.setObjectNotExistsAsync(element.vin, {
+                            type: "device",
+                            common: {
+                                name: element.modelDescription,
+                                role: "indicator",
+                            },
+                            native: {},
+                        });
+
+                        this.extractKeys(this, element.vin + ".general", element);
+                    });
+                })
+                .catch((error) => {
+                    this.log.error(error);
+                    this.log.error("GetVehicles failed");
+                    error.response && this.log.error(JSON.stringify(error.response.data));
+                    reject();
+                });
+        });
+    }
+    randomString(length) {
+        let result = "";
+        const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+        const charactersLength = characters.length;
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+
+    amzDate() {
+        return new Date().toISOString().slice(0, 20).replace(/-/g, "").replace(/:/g, "").replace(/\./g, "") + "Z";
+    }
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    onUnload(callback) {
+        try {
+            callback();
+            this.clearInterval(refreshTokenInterval);
+        } catch (e) {
+            callback();
+        }
+    }
+
+    /**
+     * Is called if a subscribed state changes
+     * @param {string} id
+     * @param {ioBroker.State | null | undefined} state
+     */
+    onStateChange(id, state) {
+        if (state) {
+            // The state was changed
+            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+        } else {
+            // The state was deleted
+            this.log.info(`state ${id} deleted`);
+        }
+    }
+}
+
+if (require.main !== module) {
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    module.exports = (options) => new Fiat(options);
+} else {
+    // otherwise start the instance directly
+    new Fiat();
+}
